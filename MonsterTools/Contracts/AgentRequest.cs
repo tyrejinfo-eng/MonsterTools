48e2b647c8e73a86add4e9030abcc0c452df7a61
Ty Rejinfo <ty.rejinfo@gmail.com>
2026-06-10 20:54:47 +0200

    Delete MonsterTools directory

--- MonsterTools/Contracts/AgentRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace MonsterTools.Contracts;
-
-public sealed class AgentRequest
-{
-    public string Prompt { get; set; } = "";
-    public string Workspace { get; set; } = "";
-}
--- MonsterTools/Contracts/ToolExecuteRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace MonsterTools.Contracts;
-
-public sealed class ToolExecuteRequest
-{
-    public string Tool { get; set; } = "";
-
-    public Dictionary<string, object?> Args { get; set; }
-        = new();
-}
